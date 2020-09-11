33f0c264f58c6228832e7820b30b4435bee56d15
Matt Ickstadt <mattico8@gmail.com>
2020-09-11 16:41:11 -0500

    Don't require units in VTG ublox receivers don't have units when there's no fix.

--- src/parsers/vtg.rs
+++ src/parsers/vtg.rs
@@ -5,14 +5,10 @@ use crate::parse::*;
 named!(pub (crate) parse_vtg<VtgData>,
     map_res!(
         do_parse!(
-            bearing_true: opt!(map_res!(take_until!(","), parse_num::<f32>)) >>
-            tag!(",T,") >>
-            bearing_magnetic: opt!(map_res!(take_until!(","), parse_num::<f32>)) >>
-            tag!(",M,") >>
-            speed_knots: opt!(map_res!(take_until!(","), parse_num::<f32>)) >>
-            tag!(",N,") >>
-            speed_kmh: opt!(map_res!(take_until!(","), parse_num::<f32>)) >>
-            tag!(",K*") >>
+            bearing_true: opt!(terminated!(map_res!(take_until!(","), parse_num::<f32>), tag!(",T,"))) >>
+            bearing_magnetic: opt!(terminated!(map_res!(take_until!(","), parse_num::<f32>), tag!(",M,"))) >>
+            speed_knots: opt!(terminated!(map_res!(take_until!(","), parse_num::<f32>), tag!(",N,"))) >>
+            speed_kmh: opt!(terminated!(map_res!(take_until!(","), parse_num::<f32>), tag!(",K*"))) >>
             (bearing_true, bearing_magnetic, speed_knots, speed_kmh)
         ),
         | sentence: (Option<f32>, Option<f32>, Option<f32>, Option<f32>)| -> Result<VtgData, NmeaSentenceError> {
