c75b45a08ed3a5fad42bba49c0cd4c1019fbbfe5
Matt Ickstadt <mattico8@gmail.com>
2021-04-05 15:58:03 -0500

    Disable sentences which require borrowing

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 mod utils;
-pub(crate) mod bod;
-pub(crate) mod bwc;
+// pub(crate) mod bod;
+// pub(crate) mod bwc;
 pub(crate) mod gbs;
 pub(crate) mod gga;
 pub(crate) mod gll;
@@ -10,10 +10,10 @@ pub(crate) mod gsa;
 pub(crate) mod gsv;
 pub(crate) mod hdt;
 pub(crate) mod rma;
-pub(crate) mod rmb;
+// pub(crate) mod rmb;
 pub(crate) mod rmc;
 pub(crate) mod stn;
 pub(crate) mod vbw;
 pub(crate) mod vtg;
-pub(crate) mod wpl;
+// pub(crate) mod wpl;
 pub(crate) mod pubx;
